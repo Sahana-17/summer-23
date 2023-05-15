6ddc5a86d9238fac8963abc4c4b04df676f064bf
Sahana Muralikrishnan <hola.sahana@gmail.com>
2023-05-15 18:55:17 +0400

    day 1

---
+++ python23/last_element.py
@@ -0,0 +1,15 @@
+#Day1
+
+def find_element(one):
+
+    if not one:
+        return None
+    else:
+        return one[-1]
+    
+
+example_list = ['a', 'b', 'c', 'd']
+last_element = find_element(example_list)
+
+print("Last element : ", last_element)
+
