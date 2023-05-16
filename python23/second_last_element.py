9c84e1b884a5d3a01b7aab05358bb3fb915300cf
Sahana Muralikrishnan <hola.sahana@gmail.com>
2023-05-16 15:36:45 +0400

    day 2 python

---
+++ python23/second_last_element.py
@@ -0,0 +1,14 @@
+#Day2
+
+def find_element(one):
+    if len(one) < 2:
+        return None
+    
+    return one[-2]
+    
+
+example_list = ['a', 'b', 'c', 'd']
+second_last_element = find_element(example_list)
+
+print("Second Last Element : ", second_last_element)
+
