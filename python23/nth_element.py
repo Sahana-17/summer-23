a001fe5aeb2733a78aa24635dce5e217eb55e46f
Sahana Muralikrishnan <hola.sahana@gmail.com>
2023-05-16 16:29:51 +0400

    problem 3 python

---
+++ python23/nth_element.py
@@ -0,0 +1,16 @@
+#Problem3
+
+def find_element(one, n):
+    if n < len(one):
+        return one[n-1]
+    else:
+        return None
+        
+
+example_list = ['a', 'b', 'c', 'd', 'e']
+term = int(input("Enter n : "))
+
+nth_element = find_element(example_list, term)
+
+print("Element : ", nth_element)
+
