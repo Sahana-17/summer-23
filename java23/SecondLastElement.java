898843ad4259dabb671628ad162729e653dc1170
Sahana Muralikrishnan <hola.sahana@gmail.com>
2023-05-16 15:37:17 +0400

    day 2 java

---
+++ java23/SecondLastElement.java
@@ -0,0 +1,19 @@
+//Day 2
+
+package java23;
+import java.util.List;
+
+public class SecondLastElement {
+    public static void main(String[] args) {
+        List<String> list = List.of("a", "b", "c", "d");
+
+        if (list.isEmpty()) {
+            System.out.println("Empty List");
+        } else {
+            String secondlastElement = list.get(list.size() - 2);
+            System.out.println("Second Last Element : " + secondlastElement);
+        }
+    }
+    
+}
+
