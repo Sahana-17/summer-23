945a633a49df39cb52e2356c50d013cc8a92d7e3
Sahana Muralikrishnan <hola.sahana@gmail.com>
2023-05-25 17:06:23 +0400

    problem 9 alternate python

---
+++ python23/sublist_groupby.py
@@ -0,0 +1,11 @@
+from itertools import groupby
+
+def pack(sample_list):
+    sublist = []
+    for key, group in groupby(sample_list):
+         sublist.append(list(group))
+
+    return sublist
+
+example_list = ['a', 'a', 'a', 'b', 'b', 'c', 'b', 'b', 'd']
+print(pack(example_list))
